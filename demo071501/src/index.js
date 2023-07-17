66804a95657651c7aac63f354e90fc23a83bc67f
web-ljc <aaaa4l@qq.com>
2023-07-17 17:25:04 +0800

    feat: MVC模式和MVVM模式

--- demo071501/src/index.js
+++ demo071501/src/index.js
@@ -3,8 +3,27 @@ import ReactDOM from 'react-dom/client';
 import '@/index.less';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+class Number extends React.Component {
+    state = {
+        num: 0
+    }
+
+    render() {
+        let { num } = this.state;
+        return <>
+            <span>{num}</span>
+            <div onClick={() => {
+                this.setState({
+                    num: num + 1
+                })
+            }}>累加</div>
+        </>
+    }
+}
+
 root.render(
-    <div>12399</div>
+    <Number></Number>
 );
 
 fetch('/jian/subscriptions/recommended_collections')
