b7b3f4e73ce75f8985668a71cd31cb5d25158b80
web-ljc <aaaa4l@qq.com>
2023-08-19 18:19:33 +0800

    feat: useReducer对状态管理

--- demo0818/src/views/a/A1.jsx
+++ demo0818/src/views/a/A1.jsx
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useReducer } from "react";
+
+// 初始状态
+const initialState = {
+    num: 0
+}
+// 创建局部store容器
+const reducer = (state, action) => {
+    state = {...state}
+    switch(action.type) {
+        case 'plus':
+            state.num++
+            break
+        case 'minus':
+            state.num--
+            break
+        default:
+    }
+    return state // 返回的状态信息
+}
 
 const A1 = () => {
-    return <>
-        A1
-    </>
+    const [state, dispatch] = useReducer(reducer, initialState)
+    console.log(state, 99);
+    return <div className="box">
+        <span>{state.num}</span>
+        <br />
+        <button onClick={() => {
+            dispatch({type: 'plus'})
+        }}>add</button>
+        <button onClick={() => {
+            dispatch({type: 'minus'})
+        }}>delete</button>
+    </div>
 }
 
 export default A1
